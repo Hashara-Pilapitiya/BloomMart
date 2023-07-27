871fa581518b687909c344068d4b5f1c1ed1f024
hasharanethmi2020@gmail.com <hasharanethmi2020@gmail.com>
2023-07-27 16:13:36 +0530

    changed shop page

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ if(close){
         nav.classList.remove('active');
     })
 }
+
+
+
+var MainImg = document.getElementById('main-img');
+var SmallImg = document.getElementsByClassName('small-img');
+
+SmallImg[0].onclick = function(){
+    MainImg.src = SmallImg[0].src;
+}
+
+SmallImg[1].onclick = function(){
+    MainImg.src = SmallImg[1].src;
+}
+
+SmallImg[2].onclick = function(){
+    MainImg.src = SmallImg[2].src;
+}
+
+SmallImg[3].onclick = function(){
+    MainImg.src = SmallImg[3].src;
+}
